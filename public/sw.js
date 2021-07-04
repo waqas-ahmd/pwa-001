f0fa1e9864788e5ed7bd18d66af0afc20fee3d42
waqas-ahmd <65waqas@gmail.com>
2021-07-04 14:07:10 +0500

    code update

--- public/sw.js
+++ public/sw.js
@@ -2,32 +2,34 @@ let cacheData = "cache1";
 
 this.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheData).then((cache) =>
-      cache.addAll([
-        "./",
-        "./sw.js",
-        "./index.html",
-        "./logo192.png",
-        "./logo512.png",
-        "./favicon.ico",
-        "./manifest.js",
-        "./static/css/main.88411b9e.chunk.css",
-        "./static/js/2.f935c2f7.chunk.js",
-        "./static/js/3.770260a0.chunk.js",
-        "./static/js/main.803fc70f.chunk.js",
-        "./static/js/runtime-main.4bf77563.js",
-        "./static/media/appointment.37680d70.svg",
-        "./static/media/email.f719840a.svg",
-        "./static/media/gallery.53a4e4d7.svg",
-        "./static/media/image2.7755af15.png",
-        "./static/media/image3.46b4252b.png",
-        "./static/media/left.6d13f12e.svg",
-        "./static/media/phone.58c41d5c.svg",
-        "./static/media/play.a4126d3c.svg",
-        "./static/media/share.4ece2a64.svg",
-        "./static/media/whatsapp.f02afb2f.svg",
-      ])
-    )
+    caches
+      .open(cacheData)
+      .then((cache) =>
+        cache.addAll([
+          "./",
+          "./sw.js",
+          "./index.html",
+          "./logo192.png",
+          "./logo512.png",
+          "./favicon.ico",
+          "./manifest.json",
+          "./static/css/main.88411b9e.chunk.css",
+          "./static/js/2.f935c2f7.chunk.js",
+          "./static/js/3.770260a0.chunk.js",
+          "./static/js/main.803fc70f.chunk.js",
+          "./static/js/runtime-main.4bf77563.js",
+          "./static/media/appointment.37680d70.svg",
+          "./static/media/email.f719840a.svg",
+          "./static/media/gallery.53a4e4d7.svg",
+          "./static/media/image2.7755af15.png",
+          "./static/media/image3.46b4252b.png",
+          "./static/media/left.6d13f12e.svg",
+          "./static/media/phone.58c41d5c.svg",
+          "./static/media/play.a4126d3c.svg",
+          "./static/media/share.4ece2a64.svg",
+          "./static/media/whatsapp.f02afb2f.svg",
+        ])
+      )
   );
 });
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import swDev from "./swDev";
 ReactDOM.render(
   // <Router>
-    <App />,
+  <App />,
   // </Router>,
   document.getElementById("root")
 );
@@ -16,4 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-swDev();
+// swDev();
